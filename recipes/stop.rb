eba2420c7518832a96284c784a1412c49229cdba
Noah Blumenthal <noah@hackerhasid.com>
2014-09-10 17:00:20 -0400

    typo in stop recipe

--- recipes/stop.rb
+++ recipes/stop.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
-  execute "restart-sidekiq" do
+  execute "stop-sidekiq" do
     command %Q{
       echo "monit -g sidekiq_#{application} stop all" | at now
     }
