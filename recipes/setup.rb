1b2ef3a72623dc351f6688e9dcba8b61a3318237
Noah Blumenthal <noah@hackerhasid.com>
2015-01-06 14:12:31 -0500

    use layer-specific settings before falling back on app-level settings

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,7 +8,14 @@ node[:deploy].each do |application, deploy|
     variables deploy: deploy
   end
 
-  settings = node[:sidekiq][application]
+  layer = node[:opsworks][:instance][:layers].first
+
+  # first get layer-specific settings
+  settings = node[:sidekiq][layer]
+
+  # fall back on application-specific settings
+  settings = node[:sidekiq][application] if settings.nil?
+
   # configure rails_env in case of non-rails app
   rack_env = deploy[:rails_env] || settings[:rack_env] || settings[:rails_env]
   instances = settings[:instances] || 1
